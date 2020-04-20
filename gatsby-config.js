0aaf02b278d20bcb99938929be070958feb22e5d
Yenisbel Valle <yenisbelv@gmail.com>
2020-04-20 15:47:42 -0700

    checkout component that loads Stripe.js and redirects to the checkout

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `E-commerce with Stripe`,
+    description: `Kick off your next, great e-commerce project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
   plugins: [
